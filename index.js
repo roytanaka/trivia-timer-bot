dc509564acf403c01ddf4b2f499afa751bb8a0d3
Roy Tanaka <tanaka@mac.com>
2020-04-25 11:21:36 -0400

    Timer condition

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ client.on('message', async (message) => {
       if (i === 15) {
         msg.edit(':mega: TIME’S UP :alarm_clock: Submit your answer!');
         clearInterval(interval);
-      } else if (i % 5 === 0 || (i >= 12 && i <= 14)) {
+      } else if (i <= 14) {
         msg.edit(':mega: Submit your answer in ' + emojis[i] + ' seconds!');
       }
       i++;
