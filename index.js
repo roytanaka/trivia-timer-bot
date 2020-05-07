506d4dff84ef453aee0ac8e64d4d8b07a5c76fcc
Roy Tanaka <tanaka@mac.com>
2020-05-07 16:24:11 -0400

    get nicknames sum correct answers and post message

--- index.js
+++ index.js
@@ -50,18 +50,53 @@ client.on('message', async (message) => {
 
   // Display scores with::scores command
   if (message.content.startsWith(`${prefix}scores`)) {
-    const today = Number(new Date().setHours(0, 0, 0, 0));
+    // get nicknames of all users on server
+    const nicknames = {};
+    message.guild.members.cache.forEach((member) => {
+      nicknames[member.user.id] = member.nickname;
+    });
+    const yesterday = new Date().setDate(new Date().getDate() - 1);
     const messages = message.channel.messages.cache
       .array()
       //filter out bots and messages earlier than today
-      .filter((msg) => !msg.author.bot && msg.createdTimestamp > today);
-    console.log('log: messages', messages);
+      .filter((msg) => !msg.author.bot && msg.createdTimestamp > yesterday);
+
+    const contestants = new Map();
+    // unique contestants list
+    for (const message of messages) {
+      contestants.set(message.author.id, message.author);
+    }
+
     // filter for correct answers i.e. msg with ✅ reaction
-    const correct = messages.filter((msg) =>
+    const correctMessages = messages.filter((msg) =>
       msg.reactions.cache.find((reaction) => reaction._emoji.name === '✅')
     );
+    for (const id of contestants.keys()) {
+      const score = correctMessages.filter((msg) => msg.author.id === id);
+      const user = contestants.get(id);
+      user.score = score.length;
+      user.nickname = nicknames[id];
+      contestants.set(id, user);
+    }
 
-    const msg = await message.channel.send(`Total correct: ${correct.length}`);
+    const scores = [];
+    contestants.forEach((contestant) => {
+      scores.push({
+        score: contestant.score,
+        user: contestant.nickname || contestant.username,
+      });
+    });
+
+    scores.sort((a, b) => b.score - a.score);
+
+    let currentScores = 'Current scores:\n';
+    scores.forEach((score) => {
+      currentScores += `*${score.user}* - **${score.score}**\n`;
+    });
+    console.log(currentScores);
+
+    await message.channel.send(currentScores.trim());
+    // message.delete();
   }
 });
 
