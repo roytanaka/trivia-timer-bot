efc39b691487957556c0c86fdf738415d7755abb
Roy Tanaka <tanaka@mac.com>
2020-05-14 18:33:25 -0400

    no scores message

--- commands/scores.js
+++ commands/scores.js
@@ -1,16 +1,26 @@
 const getScores = require('../utils/tallyScores');
+const { scoreKeepers } = require('../config.json');
 
 module.exports = {
   name: 'scores',
   description: 'Tally score of every contestant, sort and post results',
   async execute(message) {
     const scores = await getScores(message);
-
-    let currentScores = 'Current scores:\n';
-    scores.forEach((score) => {
-      currentScores += `*${score.user}* - **${score.score}**\n`;
-    });
-    await message.channel.send(currentScores.trim());
-    // message.delete();
+    if (scores.length) {
+      let currentScores = 'Current scores:\n';
+      scores.forEach((score) => {
+        currentScores += `*${score.user}* - **${score.score}**\n`;
+      });
+      await message.channel.send(currentScores.trim());
+    } else {
+      let noScores =
+        'No Scores Found. Mark messages with the following reactions:';
+      for (const keeper of scoreKeepers) {
+        noScores += `\n${keeper.emoji} “${keeper.name}” for ${keeper.score} ${
+          keeper.score === 1 ? 'point' : 'points'
+        }`;
+      }
+      await message.channel.send(noScores);
+    }
   },
 };
