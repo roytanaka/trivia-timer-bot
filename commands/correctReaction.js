0ba81bc81223c6622997aefdb60fe103dc1580ce
Roy Tanaka <tanaka@mac.com>
2020-12-12 01:35:54 -0500

    reject no entry sign

--- commands/correctReaction.js
+++ commands/correctReaction.js
@@ -1,19 +1,20 @@
-const { scoreKeepers } = require('../config.json');
+const { scoreKeepers, ignore } = require('../config.json');
 
 module.exports = {
   name: 'correctReaction',
   description:
     'Prevent adding ⭐️, ✅ and 🔶 for anyone without Trivia Master role',
   async execute(reaction, user) {
-    const emoji = reaction.emoji.name;
+    const reactedEmoji = reaction.emoji.name;
 
-    const isScoreKeeper = scoreKeepers
-      .map((keeper) => keeper.emoji)
-      .includes(emoji);
+    const restrictedEmojis = scoreKeepers.map(keeper => keeper.emoji);
+    restrictedEmojis.push(ignore);
 
-    if (!isScoreKeeper) return;
+    const isRestricted = restrictedEmojis.includes(reactedEmoji);
+
+    if (!isRestricted) return;
     // get Trivia Master role ID
-    const triviaRoleId = reaction.message.guild.roles.cache.find((role) => {
+    const triviaRoleId = reaction.message.guild.roles.cache.find(role => {
       return role.name.includes('TRIVIA MASTER');
     }).id;
     // Members with Trivia Master Role
@@ -22,7 +23,7 @@ module.exports = {
     if (reaction.count > 1) {
       // If already has reaction, remove user
       reaction.users.remove(user);
-    } else if (!membersWithRole.some((member) => member.id === user.id)) {
+    } else if (!membersWithRole.some(member => member.id === user.id)) {
       // Remove if not Trivia Master
       await reaction.remove();
     }
