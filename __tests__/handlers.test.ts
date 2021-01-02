32d5df82ed8b68ba60ae5718ed735b0ac8e6621b
Roy Tanaka <tanaka@mac.com>
2021-01-02 13:35:43 -0500

    additional tests

--- __tests__/handlers.test.ts
+++ __tests__/handlers.test.ts
@@ -7,6 +7,16 @@ describe('Message handler', () => {
       send: jest.fn(),
     },
     author: { bot: false },
+    content: '',
+  } as unknown) as Message;
+
+  const messageFromTMMock: Message = ({
+    channel: {
+      send: jest.fn(),
+    },
+    author: { bot: false },
+    content: '',
+    member: { roles: { cache: [{ name: 'TRIVIA MASTER' }] } },
   } as unknown) as Message;
 
   beforeEach(() => {
@@ -19,16 +29,31 @@ describe('Message handler', () => {
     expect(messageMock.channel.send).not.toHaveBeenCalled();
   });
 
-  it('should not respond to users who are Trivia Master', async () => {
+  it('should not respond on channels that dont start with "Trivia"', async () => {
+    messageMock.content = 'normal message';
+    if (messageMock.channel.type === 'text') {
+      messageMock.channel.name = 'trivia-channel';
+    }
+    await messageHandler(messageMock);
+    expect(messageMock.channel.send).not.toHaveBeenCalled();
+  });
+
+  it('should not respond to users who are not Trivia Master', async () => {
     messageMock.content = '::scores';
     await messageHandler(messageMock);
     expect(messageMock.channel.send).not.toHaveBeenCalled();
   });
 
   it('should not send if author is a bot', async () => {
-    messageMock.content = 'ping';
+    messageMock.content = '::scores';
     messageMock.author.bot = true;
     await messageHandler(messageMock);
     expect(messageMock.channel.send).not.toHaveBeenCalled();
+  });
+
+  it('should send message when role is TRIVIA MASTER', async () => {
+    messageFromTMMock.content = '::scores';
+    await messageHandler(messageFromTMMock);
+    expect(messageFromTMMock.channel.send).toHaveBeenCalled();
   });
 });
